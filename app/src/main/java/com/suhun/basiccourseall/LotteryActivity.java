a3b21fc046b99aa762b949804b71ce85fd0af331
suhunlin <lin.suhun@msa.hinet.net>
2023-04-24 18:53:33 +0800

    Add Lottery layout

--- app/src/main/java/com/suhun/basiccourseall/LotteryActivity.java
+++ app/src/main/java/com/suhun/basiccourseall/LotteryActivity.java
@@ -3,6 +3,7 @@ package com.suhun.basiccourseall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LotteryActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class LotteryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lottery);
+    }
+
+    public void create(View view){
+
+    }
+
+    public void exit(View view){
+        finish();
     }
 }
