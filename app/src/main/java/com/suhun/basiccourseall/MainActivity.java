5584a4a09f354e758250b2d5608b57fa103b2138
suhunlin <lin.suhun@msa.hinet.net>
2023-04-24 18:28:10 +0800

    Add LotteryActivity and implement intent and call back code

--- app/src/main/java/com/suhun/basiccourseall/MainActivity.java
+++ app/src/main/java/com/suhun/basiccourseall/MainActivity.java
@@ -1,14 +1,36 @@
 package com.suhun.basiccourseall;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public String tag = MainActivity.class.getSimpleName();
+    private ActivityResultLauncher goStartCallBalk =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            Log.d(tag, "---Go Back Main Activity---");
+
+                        }
+                    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lottery(View view){
+        Intent intent = new Intent(this, LotteryActivity.class);
+        goStartCallBalk.launch(intent);
     }
 }
