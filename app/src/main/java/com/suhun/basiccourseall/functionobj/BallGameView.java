fc6648d6f3c124a47953a33a6d10bcb006b2e310
suhunlin <lin.suhun@msa.hinet.net>
2023-04-26 13:24:09 +0800

    Add multi ball move method

--- app/src/main/java/com/suhun/basiccourseall/functionobj/BallGameView.java
+++ app/src/main/java/com/suhun/basiccourseall/functionobj/BallGameView.java
@@ -5,11 +5,14 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.suhun.basiccourseall.R;
@@ -17,24 +20,60 @@ import com.suhun.basiccourseall.R;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BallGameView extends View {
     private String tag = BallGameView.class.getSimpleName();
     private Resources res;
-    private boolean isInitBall;
     private Ball ball;
+    private int dX, dY;
     private LinkedList<Ball> balls = new LinkedList<>();
     private class Ball{
         private int[] ballId = {R.drawable.ball0, R.drawable.ball1, R.drawable.ball2, R.drawable.ball3};
         private Bitmap ballbmp;
-        private int ballW, ballH;
-        private float ballX, ballY;
+        private int ballW, ballH, viewW, viewH;
+        private float ballX, ballY, dX, dY;
+        private Timer timer = new Timer();
+        private BallTask ballTask;
+        private boolean isBallMove;
+        private class BallTask extends TimerTask{
+            @Override
+            public void run() {
+                if(ballX < 0 || (ballX + ballW) > viewW){
+                    dX *= -1;
+                }
+                if(ballY < 0 || (ballY + ballH) > viewH){
+                    dY *= -1;
+                }
+                ballX += dX;
+                ballY += dY;
+                postInvalidate();
+            }
+        }
         public Ball(Resources res,float ballX, float ballY){
+            this.viewW = getWidth(); this.viewH = getHeight();
             this.ballX = ballX; this.ballY = ballY;
-            this.ballW = (int)(getWidth()/12.0); this.ballH = ballW;
+            this.ballW = (int)(this.viewW/12.0); this.ballH = ballW;
             this.ballbmp = Bitmap.createScaledBitmap(
                     BitmapFactory.decodeResource(res, ballId[new Random().nextInt(4)])
                     ,ballW, ballH, false);
+            this.dX = this.dY = 16;
+        }
+
+        public void startBallRun(){
+            if(!this.isBallMove){
+                ballTask = new BallTask();
+                timer.schedule(ballTask, 1000, 100);
+                this.isBallMove = true;
+            }
+        }
+        public void stopBallRun() {
+            if (this.ballTask != null) {
+                this.isBallMove = false;
+                ballTask.cancel();
+                ballTask = null;
+            }
         }
     }
     public BallGameView(Context context, @Nullable AttributeSet attrs) {
@@ -42,24 +81,18 @@ public class BallGameView extends View {
         setBackgroundResource(R.drawable.ballgame_bg);
         res = context.getResources();
     }
-
-    private void initBall(){
-        isInitBall = true;
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(!isInitBall){
-            initBall();
-        }
+
         if(ball!=null){
             for(Ball ball:balls){
-                canvas.drawBitmap(ball.ballbmp, ball.ballX, ball.ballY, null);
+                ball.startBallRun();
+                canvas.drawBitmap(ball.ballbmp, ball.ballX + ball.dX, ball.ballY + ball.dY, null);
+                ball.startBallRun();
             }
         }
     }
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction()==MotionEvent.ACTION_DOWN){
@@ -67,6 +100,14 @@ public class BallGameView extends View {
             balls.add(ball);
             postInvalidate();
         }
+//        else if(event.getAction() == MotionEvent.ACTION_MOVE){
+//            if(balls.size() > 0){
+//                for(Ball ball:balls){
+//                    ball.stopBallRun();
+//                    postInvalidate();
+//                }
+//            }
+//        }
         return true;
     }
 }
